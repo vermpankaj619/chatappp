dc96e1d2e9e1a618e432403c09e9c4b28dfaa336
vermpankaj619 <vermapankaj619@gmail.com>
2018-01-23 23:24:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var socket = require('socket.io');
+
+var app = express();
+
+
+var server = app.listen(8000, function(){
+  console.log('server is running on 8000')
+}
+);
+
+app.use(express.static('public'));
+
+
+var io= socket(server);
+
+io.on('connection', function(socket){
+    
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data);
+  });
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing',data);
+  });
+});
+
